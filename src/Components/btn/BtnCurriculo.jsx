b536983087e65dd9e5721941ac138c2633cd9f03
Janilson Coimbra <janilson.coimbra@gmail.com>
2022-03-31 18:37:11 -0300

    Chore innit version

--- src/Components/btn/BtnCurriculo.jsx
+++ src/Components/btn/BtnCurriculo.jsx
@@ -1,5 +1,6 @@
 import Styles from './BtnCurriculo.module.css'
 function BtnCurriculo(){
-    return(<><button className={Styles.btnCurriculo}>Baixe o Curriculo</button></>)
+    return(<><a href='../../..\public\Docs\JanilsonCoimbraCurrículo.pdf' download><button className={Styles.btnCurriculo}>Baixe o Curriculo</button></a></>)
 }
 export default BtnCurriculo
+//C:\Users\janil\Desktop\Portifolio\public\Docs\JanilsonCoimbraCurrículo.pdf
